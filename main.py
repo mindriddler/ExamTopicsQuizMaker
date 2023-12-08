f48fd66da51c598c57bb0f15a889faeb8c96751d
Fredrik Magnusson <fredrikmagnusson3@gmail.com>
2023-12-08 13:49:31 +0100

    refactor: Refactor gitignore and main.py for exam selection and copying

--- main.py
+++ main.py
@@ -1,8 +1,82 @@
+import os
+import shutil
+
 from quiz import Quiz
 
 RES_DIR = "./res"  # directory of resources
+EXAM_DIR = "./exams"  # directory of exams
+
+
+def read_exam(exam_dir):
+    """Reads the exam from the exam directory and returns the exam as a string."""
+    exams = os.listdir(exam_dir)
+    exam_list = []
+    for exam in exams:
+        if os.path.isdir(f"{exam_dir}/{exam}"):
+            exam_list.append(exam)
+        else:
+            pass
+    return exam_list
+
+
+def choose_exam(exam_dir):
+    """Choose the exam to be read."""
+    delete_current_exam(RES_DIR)
+
+    exams = read_exam(exam_dir)
+    print("Choose the exam you want to take: ")
+    print("Available exams: ")
+
+    for i, exam in enumerate(exams, start=1):
+        print(f"{i}. {exam}")
+
+    while True:
+        try:
+            exam_choice = int(
+                input("Enter the number of the exam you want to take: "))
+            if 0 < exam_choice <= len(exams):
+                exam_choice -= 1
+                break
+            else:
+                print("Invalid number, please try again.")
+        except ValueError:
+            print("Invalid input, please enter a number.")
+
+    exam = exams[exam_choice]
+    print(f"You chose {exam}.")
+    return exam
+
+
+def copy_exam(exam_dir, exam, res_dir):
+    """Copy the exam to the resources directory."""
+    # Path to the source exam directory and the HTML file
+    src_exam_dir = os.path.join(exam_dir, exam)
+    src_html_file = os.path.join(exam_dir, f"{exam}.html")
+
+    # Path to the destination exam directory and the HTML file
+    dest_exam_dir = os.path.join(res_dir, exam)
+    dest_html_file = os.path.join(res_dir, f"{exam}.html")
+
+    # Copy the exam directory
+    if os.path.exists(dest_exam_dir):
+        shutil.rmtree(dest_exam_dir)
+    shutil.copytree(src_exam_dir, dest_exam_dir)
+
+    # Copy the HTML file
+    if os.path.exists(dest_html_file):
+        os.remove(dest_html_file)
+    shutil.copy2(src_html_file, dest_html_file)
+
+
+def delete_current_exam(res_dir):
+    """Delete the current exam."""
+    if os.path.exists(res_dir):
+        print("Folder exists. Deleting...")
+        shutil.rmtree(res_dir)
+
 
 if __name__ == "__main__":
+    chosen_exam = choose_exam(EXAM_DIR)
+    copy_exam(EXAM_DIR, chosen_exam, RES_DIR)
     quiz = Quiz(RES_DIR)
-
     quiz.start_quiz()
--- quiz.py
+++ quiz.py
@@ -1,21 +1,23 @@
-from io import TextIOWrapper
-from _classes import CardList, os, re
 import random
-from datetime import datetime 
 import textwrap
+from datetime import datetime
+from io import TextIOWrapper
+
+from _classes import CardList, os, re
+
 
 class Quiz:
-    def __init__(self, resources_dir = None) -> None:
+
+    def __init__(self, resources_dir=None) -> None:
         self.__cardlist = CardList(resources_dir)
         self.__log_dirname = "wrong_answers"  # the name of the directory where the .txt files with wrong answers will be stored
-        
-        self.__init_questions_per_quiz()        
-        self.__init_show_answer_immediately() 
+
+        self.__init_questions_per_quiz()
+        self.__init_show_answer_immediately()
 
         self.__create_wrong_answers_directory()
-        
-        self.quiz_cards = self.__generate_quiz()
 
+        self.quiz_cards = self.__generate_quiz()
 
     def __init_questions_per_quiz(self):
         """
@@ -26,7 +28,9 @@ class Quiz:
             self.__questions_per_quiz = \
                 int(input("How many questions do you want to have? (Max: " + str(len(self.__cardlist.cards_list)) + ") "))
 
-            while type(self.__questions_per_quiz) != int or self.__questions_per_quiz > len(self.__cardlist.cards_list):
+            while type(self.__questions_per_quiz
+                       ) != int or self.__questions_per_quiz > len(
+                           self.__cardlist.cards_list):
                 self.__questions_per_quiz = \
                     int(input("Please pick a NUMBER. (Max: " + str(len(self.__cardlist.cards_list)) + ")"))
         except:
@@ -47,8 +51,8 @@ class Quiz:
         while self.__show_answer_immediately != "n" and self.__show_answer_immediately != "y":
             self.__show_answer_immediately = \
                 input("Please pick between 'y'(yes) or 'n'(no): ")
-            self.__show_answer_immediately = self.__show_answer_immediately.lower()
-
+            self.__show_answer_immediately = self.__show_answer_immediately.lower(
+            )
 
     def __generate_quiz(self) -> list:
         """
@@ -57,7 +61,6 @@ class Quiz:
         """
         random.shuffle(self.__cardlist.cards_list)
         return self.__cardlist.cards_list[:self.__questions_per_quiz]
-
 
     def __clear(self):
         """
@@ -69,45 +72,50 @@ class Quiz:
             _ = os.system('cls')
         # for mac and linux(here, os.name is 'posix')
         else:
-            _ = os.system('clear')    
-
+            _ = os.system('clear')
 
     def __create_wrong_answers_directory(self):
         try:
             os.mkdir(self.__log_dirname)
         except:
             print("Wrong answers directory already exists. Continuing..")
-            
+
     def __init_answers_file(self) -> TextIOWrapper:
         """
             Initialize the filename with the current datetime, while omitting spaces and colon
         """
-        filename = re.sub(" ", "_", str(datetime.now())).split(".")[0]  # remove the miliseconds as they were delimited by '.'
-        filename = re.sub(":", "-", filename)  # remove ':' as they are a special char on Windows.. 
+        filename = re.sub(" ", "_", str(datetime.now())).split(".")[
+            0]  # remove the miliseconds as they were delimited by '.'
+        filename = re.sub(
+            ":", "-",
+            filename)  # remove ':' as they are a special char on Windows..
         filename += ".txt"
         filename = os.path.join(self.__log_dirname, filename)
-        wrong_answers_file = open(filename, "w")  # file where the wrong answers will be written to
+        wrong_answers_file = open(
+            filename, "w")  # file where the wrong answers will be written to
 
         return wrong_answers_file
 
-
     def __write_to_file(self, wrong_answers_file, card, your_answer):
-        
+
         wrapper = textwrap.TextWrapper()  # wrap text so it looks better
 
-        wrong_answers_file.write(card.question_number + " " + wrapper.fill(text= card.question) + "\n")
+        wrong_answers_file.write(card.question_number + " " +
+                                 wrapper.fill(text=card.question) + "\n")
         wrong_answers_file.write("-" * 40 + "\n")
         for ans in card.answers:
             try:
                 # ans = str(ans.encode('utf-8'))  # some answers give a UnicodeEncodeError: 'charmap' codec can't encode character '\u05d2' in position 192: character maps to <undefined>
-                wrong_answers_file.write(wrapper.fill(text= ans) + "\n")  # one answer had a weird encoding
+                wrong_answers_file.write(
+                    wrapper.fill(text=ans) +
+                    "\n")  # one answer had a weird encoding
             except:
                 wrong_answers_file.write(str(ans) + "\n")
 
         wrong_answers_file.write("Your answer: " + your_answer.upper() + "\n")
-        wrong_answers_file.write("Correct answer: " + card.correct_answer + "\n")
+        wrong_answers_file.write("Correct answer: " + card.correct_answer +
+                                 "\n")
         wrong_answers_file.write("-" * 40 + "\n\n")
-
 
     def start_quiz(self):
         """ 
@@ -119,17 +127,20 @@ class Quiz:
 
         wrong_answers_file = self.__init_answers_file()
         wrapper = textwrap.TextWrapper()  # wrap text so it looks better
-        
-        print("Your quiz starts now. Please enter one single character, coresponding to the answers (A,B,C or D). Answers are NOT case sensitive, so response 'b' is good if 'B' is the correct answer.\n")
+
+        print(
+            "Your quiz starts now. Please enter one single character, coresponding to the answers (A,B,C or D). Answers are NOT case sensitive, so response 'b' is good if 'B' is the correct answer.\n"
+        )
         input("Press Enter to continue..")
 
         for index, card in enumerate(self.quiz_cards):
             print("")
             print(str(index + 1) + "/" + str(self.__questions_per_quiz))
-            print(card.question_number + " " + wrapper.fill(text= card.question))
+            print(card.question_number + " " +
+                  wrapper.fill(text=card.question))
             print("-" * 40)
             for ans in card.answers:
-                print(wrapper.fill(text= ans))
+                print(wrapper.fill(text=ans))
             print("-" * 40)
             your_answer = ""
             while your_answer.upper() not in ['A', 'B', 'C', 'D']:
@@ -140,13 +151,14 @@ class Quiz:
             else:
                 # write to the wrong answer to the file
                 self.__write_to_file(wrong_answers_file, card, your_answer)
-                            
+
             if self.__show_answer_immediately == "y":
                 print("Correct answer: ", card.correct_answer)
                 input("Press Enter to continue..")
 
         wrong_answers_file.close()  # writing is done so we close the file
-        
+
         print("=^=" * 40)
         print("The quiz is DONE! Good job!")
-        print("Your score: " + str(correct_answers) + "/" + str(self.__questions_per_quiz))
+        print("Your score: " + str(correct_answers) + "/" +
+              str(self.__questions_per_quiz))
